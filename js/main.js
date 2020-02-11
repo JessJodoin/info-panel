9e7f168ab344456a7e52042b9602470011d66da9
Jessica Jodoin <jodo0083@algonquinlive.com>
2020-02-11 09:29:35 -0500

    Make it do the thing

--- js/main.js
+++ js/main.js
@@ -23,3 +23,13 @@ var id = $(this).attr('href');
 });
 
 */
+
+$('.info-panel-btn').on('click', function () {
+  var id = $(this).attr('href');
+  var isOpen = $(this).hasClass('info-panel-btn-active');
+  $('.info-panel').attr('hidden', true);
+  $('.info-panel-btn').removeClass('info-panel-btn-active');
+  if (!isOpen) {
+    $(this).addClass('info-panel-btn-active');
+    $(id).removeAttr('hidden');}
+});
